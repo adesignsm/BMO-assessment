9428887ce1d25c0e3d6c8589baada80f33e3baf1
Admin <admin@Admins-MacBook-Pro-2.local>
2022-03-02 19:17:57 -0800

    assessment complete

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, Fragment} from "react";
 import axios from "axios";
 
 const BOOK_API = process.env.REACT_APP_STARTER_API;
@@ -7,85 +7,119 @@ const COVER_API = process.env.REACT_APP_BOOK_COVER_API;
 const defaultImage = "https://nnpbeta.wustl.edu/img/bookCovers/genericBookCover.jpg";
 
 const Search = () => {
-    const [searchInput, setSearchInput] = useState("");
-    const [bookResults, setBookResults] = useState([]);
+    const [searchInput, setSearchInput] = useState("");//search input state
+    const [bookResults, setBookResults] = useState([]);//book data state
+    const [dataStatus, setDataStatus] = useState(false);//loading state
 
-    const [dataStatus, setDataStatus] = useState(false); //loading status
-    const [searchStatus, setSearchStatus] = useState(false); //search status
+    const [showRawData, setShowRawData] = useState(false);//raw data state
+    const [showAlphabeticalData, setShowAlphabeticalData] = useState(false);//alphabetically filtered data
+    const [showPublishYearData, setShowPublishYearData] = useState(false);//data filtered by publisj year
 
     useEffect(() => {
         queryData();
     }, [bookResults]);
 
+    //Query the API with axios
     const queryData = async () => {
 
-        await axios.get(`${BOOK_API}${searchInput}`)
+       await axios.get(`${BOOK_API}${searchInput}`)
         .then(response => {
 
             const data = response.data.docs;
-            console.log(data.length);
             setBookResults(data);
             
             if (bookResults.length > 0) {
                 setDataStatus(true);
-                setSearchStatus(false);
             } else {
                 setDataStatus(false);
-                setSearchStatus(true);
             }
 
         }, reason => {
-            window.location.reload();
             console.error(reason);
         })
     }
 
+    //returns raw data
+    const RawData = bookResults.map(book => { 
+            return <ul key = {book.key}>
+                <h1> {book.title}</h1>
+                <img alt = {book.title + "cover image"} className = "book-cover" src = { book.cover_i ? `${COVER_API}${book.cover_i}.jpg` : defaultImage}/>
+                <li> Author: {book.author_name ? book.author_name[0] : null}</li>
+                <li> Published on {book.publish_date[0]}</li> 
+            </ul>
+        });
+    
+    //returns filtered data by alphabetical order
+    const AlphabeticalData = bookResults.sort((book1, book2) => {
+        if (book1.title.toLowerCase() < book2.title.toLowerCase()) return -1;
+        if (book1.title.toLowerCase() > book2.title.toLowerCase()) return 1;
+        return 0;
+    }).map(book => {
+        return <ul key = {book.key}>
+            <h1> {book.title}</h1>
+            <img alt = {book.title + "cover image"} className = "book-cover" src = { book.cover_i ? `${COVER_API}${book.cover_i}.jpg` : defaultImage}/>
+            <li> Author: {book.author_name ? book.author_name[0] : null}</li>
+            <li> Published on {book.publish_date[0]}</li> 
+        </ul>
+    })
+
+    //returns data filtered by publish date
+    const PublishYearData = bookResults.sort((book1, book2) => {
+        return book1.publish_year[0] - book2.publish_year[0];
+    }).map(book => {
+        return <ul key = {book.key}>
+            <h1> {book.title}</h1>
+            <img alt = {book.title + "cover image"} className = "book-cover" src = { book.cover_i ? `${COVER_API}${book.cover_i}.jpg` : defaultImage}/>
+            <li> Author: {book.author_name ? book.author_name[0] : null}</li>
+            <li> Published on {book.publish_date[0]}</li> 
+        </ul>
+    })
+    
+    //handle user search input
     const handleInput = (e) => {
 
         if (e.key === "Enter") {    
 
+            setShowRawData(true);
             setSearchInput(e.target.value);
-            e.target.value = "";
         }
     }
 
-    // const filteredData = () => {
+    //Data filter click events
+    const DataFilterClick = () => {
+        setShowAlphabeticalData(true);
+        setShowRawData(false); 
+        setShowPublishYearData(false);
+    };
 
-    //     console.log("clicked");
-
-    //     queryData();
-
-    //     bookResults.sort((book1, book2) => {
-    //         if (book1.title.toLowerCase() < book2.title.toLowerCase()) return -1;
-    //         if (book1.title.toLowerCase() > book2.title.toLowerCase()) return 1;
-    //         return 0;
-    //     }).map((book) => {
-    //        return <ul key = {book.key}>
-    //             <h1> {book.title}</h1>
-    //             <img className = "book-cover" src = { book.cover_i ? `${COVER_API}${book.cover_i}.jpg` : defaultImage}/>
-    //             <li> Author: {book.author_name[0]}</li>
-    //             <li> Published on {book.publish_date[0]}</li> 
-    //         </ul>
-    //     })
-    // }
+    const PublishYearFilterClick = () => {
+        setShowPublishYearData(true); 
+        setShowRawData(false)
+        setShowAlphabeticalData(false);
+    };
 
     return (
-        <> 
+        <Fragment> 
             <h1>Search for a book title</h1> 
-            <input type = "text" placeholder = "Enter a book" onKeyPress = {e => {handleInput(e)}}/>
-            <button> Sort by Alphabetical Order </button>
-
-            {bookResults.length > 0 ? (bookResults.map((book) => {
-                return <ul key = {book.key}>
-                    <h1> {book.title}</h1>
-                    <img className = "book-cover" src = { book.cover_i ? `${COVER_API}${book.cover_i}.jpg` : defaultImage}/>
-                    <li> Author: {book.author_name ? book.author_name[0] : null}</li>
-                    <li> Published on {book.publish_date[0]}</li> 
-                </ul>
-            })) :  <h1 style = {{display: searchStatus ? "block" : "none"}}> Sorry that search does not exist </h1>}
+            <input 
+                required 
+                autoFocus type = "text"
+                placeholder = "Enter a book"
+                aria-required = "true" 
+                aria-label = "enter book title"  
+                aria-placeholder = "Enter a book" 
+                onKeyPress = {e => {handleInput(e)}}
+            />
+            <button aria-label = "click to sort alphabetically" onClick = {DataFilterClick}> alphabetical order </button>
+            <button aria-label = "click to sort by published year" onClick = {PublishYearFilterClick}> recent publish year </button>
+            <div className = "book-data">
+                {showRawData === true && RawData}
+                {showAlphabeticalData === true && AlphabeticalData}
+                {showPublishYearData  === true && PublishYearData}
+            </div>
 
             <h1 style = {{display: dataStatus ? "block" : "none"}}> Loading Results </h1>
-        </>
+        </Fragment>
     )
 }
 
